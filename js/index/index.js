8e3bf95ceaf0692e2d0a30ac3348a641bd7ea296
ArtlessAvian <ArtlessAvian@gmail.com>
2018-01-15 06:36:06 -0800

    Bunch of Text Added
    got busted for staying up until 6:30. i guess it was worth it.

--- js/index/index.js
+++ js/index/index.js
@@ -1,22 +1,23 @@
 
 var messages = [
-    "wondering whens melee",
+    "whens melee",
     "did someone say starcraft 2",
-    "probably should sleep more",
-    "upcoming chess grandmaster",
+    "chess grandmaster",
     "what's an airport again",
     "fixing horrendous posture",
-    "is it the weekend yet"
+    "boom tetris for jeff",
+    "stop stalking me",
+    "i write games!"
 ]
 
 var period = 7000;
-var lastRun = 0;
+var lastRun = -1;
 
 var coolThing = function()
 {
     var now = Date.now();
-    $("#cool-thing").css("opacity", Math.abs(Math.sin(now / period * Math.PI)));
-    if (lastRun % period > now % period)
+    $("#cool-thing").css("opacity", Math.min(1, 3 * Math.abs(Math.sin(now / period * Math.PI))));
+    if (lastRun % period > now % period || lastRun === -1)
     {
         $("#cool-thing").text(messages[Math.floor(now / period) % messages.length]);
     }
@@ -25,4 +26,4 @@ var coolThing = function()
 }
 
 $("#cool-thing").text(messages[Math.floor(Date.now() / period) % messages.length]);
-setInterval(coolThing, 1000/60);
+setInterval(coolThing, 1000/24);
